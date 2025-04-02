bba648b2a63eae3f69abda26efea066053478a25
Alexandra Matveeva <sm1991611@gmail.com>
2025-04-02 18:04:51 +0700

    adding a patient log model

--- app/Http/Controllers/Log/LogController.php
+++ app/Http/Controllers/Log/LogController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Log;
 
 use App\Http\Controllers\Controller;
+use App\Models\Log\Log;
 use App\Models\Patient;
+use Illuminate\Http\Request;
 
 class LogController extends Controller
 {
@@ -11,5 +13,56 @@ class LogController extends Controller
     {
         return view('logShow')->with('log',Patient::query()->findOrFail($id));
     }
+
+    public function store(Request $request) : object
+    {
+
+        $request->validate([
+            'patient_id' => 'required|exists:patients,id',
+            'log_receipt_id' => 'required|exists:log_receipts,id',
+            'log_discharge_id' => 'nullable|exists:log_discharges,id',
+            'log_reject_id' => 'nullable|exists:log_rejects,id',
+        ]);
+
+        Log::query()->create($request->all());
+
+        return redirect()->to('/home');
+    }
+
+    public function update(Request $request, $id) : object
+    {
+        $log = Log::all()->findOrFail($id);
+
+        $request->validate([
+            'patient_id' => 'required|exists:patients,id',
+            'log_receipt_id' => 'required|exists:log_receipts,id',
+            'log_discharge_id' => 'nullable|exists:log_discharges,id',
+            'log_reject_id' => 'nullable|exists:log_rejects,id',
+        ]);
+
+        $log->update($request->all());
+
+        return redirect()->to('/home');
+    }
+
+    public function destroy($id) : object
+    {
+        $log = Log::all()->findOrFail($id);
+        $log->delete();
+
+        return redirect()->to('/home');
+    }
+
+//    public function index() : object
+//    {
+//        $logs = Log::all();
+//        return redirect();
+//    }
+
+//    public function show($id)
+//    {
+//        $log = Log::findOrFail($id);
+//        return response()->json($log);
+//    }
 
 }
--- app/Models/Log/Log.php
+++ app/Models/Log/Log.php
@@ -2,9 +2,41 @@
 
 namespace App\Models\Log;
 
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Log extends Model
 {
+    use HasFactory;
 
+    protected $table = 'logs';
+
+    protected $fillable = [
+        'patient_id',
+        'log_receipt_id',
+        'log_discharge_id',
+        'log_reject_id',
+    ];
+
+    public function patient() : BelongsTo
+    {
+        return $this->belongsTo(Patient::class, 'patient_id');
+    }
+
+    public function logReceipt() : BelongsTo
+    {
+        return $this->belongsTo(LogReceipt::class, 'log_receipt_id');
+    }
+
+    public function logDischarge() : BelongsTo
+    {
+        return $this->belongsTo(LogDischarge::class, 'log_discharge_id');
+    }
+
+    public function logReject() : BelongsTo
+    {
+        return $this->belongsTo(LogReject::class, 'log_reject_id');
+    }
 }
