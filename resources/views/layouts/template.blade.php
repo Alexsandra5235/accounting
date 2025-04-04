b9ee6d82bc5810d04de1895928bbb4f3c44b4a18
Alexandra Matveeva <sm1991611@gmail.com>
2025-04-04 18:58:20 +0700

    show the search bar on the required page

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -37,21 +37,23 @@
         @else
 
             <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-                <form action="/" method="get" autocomplete="off" class="d-flex me-5">
-                    <div class="input-group me-2">
-                        <input class="form-control pe-xxl-5 text-left" name="full_name" id="full_name" type="text" placeholder="Поиск пациента по ФИО">
-                        <a href="/" class="btn btn-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                            </svg>
-                        </a>
-                        <button class="btn btn-primary" type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg>
-                        </button>
-                    </div>
-                </form>
+                @if(Route::currentRouteName() == 'home')
+                    <form action="/" method="get" autocomplete="off" class="d-flex me-5">
+                        <div class="input-group me-2">
+                            <input class="form-control pe-xxl-5 text-left" name="full_name" id="full_name" type="text" placeholder="Поиск пациента по ФИО">
+                            <a href="/" class="btn btn-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                                </svg>
+                            </a>
+                            <button class="btn btn-primary" type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
+                            </button>
+                        </div>
+                    </form>
+                @endif
                 <a class="me-4 py-2 link-body-emphasis text-decoration-none top-50" href="{{ route('log.add') }}">Добавить пациента</a>
                 <a class="me-4 py-2 link-body-emphasis text-decoration-none" href="/register">Справочная информация</a>
                 <div class="dropdown py-2 me-4">
