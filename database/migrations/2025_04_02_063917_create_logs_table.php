fb69d20880dfe446aab714fd95aa68bb9b9b1f7d
Alexandra Matveeva <sm1991611@gmail.com>
2025-04-05 19:31:01 +0700

    redefining table relation

--- database/migrations/2025_04_02_063917_create_logs_table.php
+++ database/migrations/2025_04_02_063917_create_logs_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('logs', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
-            $table->timestamps();
-        });
         Schema::create('log_receipts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('log_id')->constrained()->onDelete('cascade');
             $table->date('date_receipt');
             $table->string('time_receipt');
             $table->dateTime('datetime_alcohol')->nullable();
@@ -32,7 +26,6 @@ return new class extends Migration
 
         Schema::create('log_discharges', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('log_id')->constrained('logs')->onDelete('cascade');
             $table->dateTime('datetime_discharge')->nullable();
             $table->dateTime('datetime_inform')->nullable();
             $table->string('outcome')->nullable();
@@ -42,10 +35,48 @@ return new class extends Migration
 
         Schema::create('log_rejects', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('log_id')->constrained('logs')->onDelete('cascade');
             $table->string('reason_refusal')->nullable();
             $table->string('name_medical_worker')->nullable();
             $table->text('add_info')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('classifiers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->string('value')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('diagnoses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('state_id')->constrained('classifiers')->onDelete('cascade');
+            $table->foreignId('wound_id')->constrained('classifiers')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('diagnosis_id')->constrained('diagnoses')->onDelete('cascade');
+            $table->string('name');
+            $table->date('birth_day');
+            $table->string('gender');
+            $table->string('medical_card');
+            $table->string('passport')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('address')->nullable();
+            $table->string('register_place')->nullable();
+            $table->string('snils')->nullable();
+            $table->string('polis')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('log_receipt_id')->constrained('log_receipts')->onDelete('cascade');
+            $table->foreignId('log_discharge_id')->constrained('log_discharges')->onDelete('cascade');
+            $table->foreignId('log_reject_id')->constrained('log_rejects')->onDelete('cascade');
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -55,9 +86,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('logs');
         Schema::dropIfExists('log_rejects');
         Schema::dropIfExists('log_discharges');
         Schema::dropIfExists('log_receipts');
+        Schema::dropIfExists('classifiers');
+        Schema::dropIfExists('diagnoses');
+        Schema::dropIfExists('patients');
+        Schema::dropIfExists('logs');
     }
 };
